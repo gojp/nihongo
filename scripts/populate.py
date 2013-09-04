7e24bd033e88537bb4247f5a9f88fb344896024b
Shawn Smith <shawnpsmith@gmail.com>
2013-09-04 23:27:10 +0900

    add populate.py

---
+++ scripts/populate.py
@@ -0,0 +1,18 @@
+from edict_parser import EdictEntry
+from pymongo import Connection
+
+MONGO_URI = ''
+c = Connection(MONGO_URI)
+
+mongo_db = c['greenbook']
+collection = mongo_db['edict']
+
+PATH_TO_EDICT2 = ''
+
+inserts = []
+with open(PATH_TO_EDICT2) as f:
+    read_data = f.readlines()
+    for line in [l.decode('EUC-JP') for l in read_data]:
+        inserts.append(EdictEntry(line))
+
+collection.insert(inserts)
