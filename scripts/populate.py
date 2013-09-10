d134ab2d479af4076951e09a6bbee26027e0aae9
Herman Schaaf <hermanschaaf@gmail.com>
2013-09-10 22:28:38 +0900

    Parse info and save tags in mongo db

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,21 +1,21 @@
 from edict_parser import EdictEntry
 from pymongo import Connection
+import romkan
 
-MONGO_URI = ''
+MONGO_URI = 'localhost'
 c = Connection(MONGO_URI)
 
 mongo_db = c['greenbook']
 collection = mongo_db['edict']
 
-PATH_TO_EDICT2 = ''
+PATH_TO_EDICT2 = '../data/edict'
 
 inserts = []
 with open(PATH_TO_EDICT2) as f:
     read_data = f.readlines()
     for line in [l.decode('EUC-JP') for l in read_data]:
-        inserts.append(EdictEntry(line).__dict__)
-
-for i in inserts:
-    del(i['tags'])
+    	d = EdictEntry(line).to_dict()
+    	d['romaji'] = romkan.to_roma(d['furigana'])
+        inserts.append(d)
 
 collection.insert(inserts)
