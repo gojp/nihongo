47a7b216b8def197c648368bf01ab04cbe547875
Shawn Smith <shawnpsmith@gmail.com>
2013-09-14 21:39:01 +0900

    temporarily comment out mgo functionality in app.go

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,49 +2,53 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/gojp/greenbook/app/models"
-	"github.com/jgraham909/revmgo"
+	//"github.com/gojp/nihongo/app/models"
+	//"github.com/mattbaird/elastigo/api"
+	//"github.com/mattbaird/elastigo/search"
+	//"github.com/mattbaird/elastigo/core"
 	"github.com/robfig/revel"
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
 )
 
 type App struct {
 	*revel.Controller
-	revmgo.MongoController
 }
 
-func (a App) Search(search string) revel.Result {
-	collection := a.MongoSession.DB("greenbook").C("edict")
+func (a App) Search(query string) revel.Result {
+	//api.Domain = "localhost"
+	//out, err := search.Search("edict").Type("entry").Size("100").Search(query).Result()
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(out)
 
-	fmt.Println("Searching for... ", search)
+	fmt.Println("Searching for... ", query)
 
 	// Index - not the best place for this, but okay for now...
-	index := mgo.Index{
-		Key:        []string{"romaji", "furigana", "japanese", "glosses"},
-		Unique:     false,
-		DropDups:   false,
-		Background: true,
-		Sparse:     true,
-	}
+	//index := mgo.Index{
+	//	Key:        []string{"romaji", "furigana", "japanese", "glosses"},
+	//	Unique:     false,
+	//	DropDups:   false,
+	//	Background: true,
+	//	Sparse:     true,
+	//}
 
-	err := collection.EnsureIndex(index)
+	//err := collection.EnsureIndex(index)
 
-	if err != nil {
-		panic("Database connection failed")
-	}
+	//if err != nil {
+	//	panic("Database connection failed")
+	//}
 
-	wordList := []models.Word{}
-	query := bson.M{"$or": []bson.M{
-		bson.M{"romaji": bson.RegEx{".*" + search + ".*", "i"}},
-		bson.M{"furigana": search},
-		bson.M{"japanese": search},
-	}}
-	q := collection.Find(query).Sort("-common", "furigana")
-	iter := q.Limit(100).Iter()
-	iter.All(&wordList)
+	//wordList := []models.Word{}
+	//query := bson.M{"$or": []bson.M{
+	//	bson.M{"romaji": bson.RegEx{".*" + search + ".*", "i"}},
+	//	bson.M{"furigana": search},
+	//	bson.M{"japanese": search},
+	//}}
+	//q := collection.Find(query).Sort("-common", "furigana")
+	//iter := q.Limit(100).Iter()
+	//iter.All(&wordList)
 
-	return a.Render(wordList)
+	return a.Render()
 }
 
 func (c App) Index() revel.Result {
