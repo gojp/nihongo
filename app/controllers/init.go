e8cfa06ffe76d763b200ad452910771dc4cb8e2b
Herman Schaaf <hermanschaaf@gmail.com>
2013-09-17 22:00:37 +0900

    Bunch of bug fixes
    Including the recursive loading one
    And favicon
    And robots.txt
    And basic sitemap, to be improved later

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,4 +1,7 @@
 package controllers
 
+import "github.com/robfig/revel"
+
 func init() {
+	revel.TemplateFuncs["add"] = func(a, b int) int { return a + b }
 }
--- scripts/get_edict2.sh
+++ scripts/get_edict2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p /data
-wget http://ftp.monash.edu.au/pub/nihongo/edict2.gz -O /data/edict2.gz
-gunzip /data/edict2.gz
+mkdir -p ../data
+wget http://ftp.monash.edu.au/pub/nihongo/edict2.gz -O ../data/edict2.gz
+gunzip ../data/edict2.gz
