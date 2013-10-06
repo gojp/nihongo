50c9de7caf39fcc6d8860c9d67b3abd32dbad515
Shawn Smith <shawnpsmith@gmail.com>
2013-10-06 18:07:58 +0900

    add html template function

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,7 +1,9 @@
 package controllers
 
 import "github.com/robfig/revel"
+import "html/template"
 
 func init() {
 	revel.TemplateFuncs["add"] = func(a, b int) int { return a + b }
+	revel.TemplateFuncs["html"] = func(s string) template.HTML { return template.HTML(s) }
 }
