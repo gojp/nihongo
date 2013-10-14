c7d12c8f81987379f2ac9dd2a9073f33465fdd67
Shawn Smith <shawnpsmith@gmail.com>
2013-10-14 16:14:17 +0900

    remove references to mgo

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"github.com/jgraham909/revmgo"
 	"github.com/robfig/revel"
 	"html/template"
 )
@@ -10,5 +9,4 @@ func init() {
 	revel.TemplateFuncs["add"] = func(a, b int) int { return a + b }
 	revel.TemplateFuncs["get"] = func(a []string, b int) string { return a[b] }
 	revel.TemplateFuncs["html"] = func(s string) template.HTML { return template.HTML(s) }
-	revmgo.ControllerInit()
 }
