8c10158d847b70b2707a42fb94b99fb80956e000
Shawn Smith <shawnpsmith@gmail.com>
2013-10-22 20:54:57 +0900

    add users model

---
+++ app/models/user.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"fmt"
+	"github.com/robfig/revel"
+	"regexp"
+)
+
+type User struct {
+	Name     string
+	Username string
+	Password []byte
+}
+
+func (u *User) String() string {
+	return fmt.Sprintf("User(%s)", u.Username)
+}
+
+var userRegex = regexp.MustCompile("^\\w*$")
+
+func (user *User) Validate(v *revel.Validation) {
+	v.Check(user.Username,
+		revel.Required{},
+		revel.Match{userRegex},
+	)
+
+	v.Check(user.Name,
+		revel.Required{},
+		revel.MaxSize{100},
+	)
+}
+
+func ValidatePassword(v *revel.Validation, password string) *revel.ValidationResult {
+	return v.Check(password,
+		revel.Required{},
+		revel.MinSize{5},
+	)
+}
