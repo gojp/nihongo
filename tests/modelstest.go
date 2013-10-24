519affe6630dc91610171a1461d0e598fbe0eacc
Herman Schaaf <hermanschaaf@gmail.com>
2013-10-25 00:11:29 +0900

    Add models tests and test HighlightQuery

---
+++ tests/modelstest.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"github.com/gojp/nihongo/app/models"
+	"github.com/robfig/revel"
+)
+
+type ModelsTest struct {
+	revel.TestSuite
+}
+
+func (s *ModelsTest) Before() {
+}
+
+func (s *ModelsTest) After() {
+}
+
+func (s *ModelsTest) TestHighlightQuery() {
+	// some basic checks
+	w := models.Word{
+		English:  []string{"test"},
+		Furigana: "テスト",
+		Japanese: "テスト",
+	}
+	w.HighlightQuery("tesuto")
+	s.Assert(w.English[0] == "test")
+	s.Assert(w.EnglishHL[0] == "test")
+	s.Assert(w.Furigana == "テスト")
+	s.Assert(w.FuriganaHL == "<strong>テスト</strong>")
+	s.Assert(w.Japanese == "テスト")
+	s.Assert(w.JapaneseHL == "<strong>テスト</strong>")
+}
