6eafc1c72df45f77f238a79478bd6f5c1f3c7a4f
Herman Schaaf <hermanschaaf@gmail.com>
2013-10-26 11:05:47 +0900

    Fix double-highlight bug for furigana and simplify logic

--- app/models/word.go
+++ app/models/word.go
@@ -36,18 +36,17 @@ func (w *Word) HighlightQuery(query string) {
 	// if the original input is Japanese, then the original input converted
 	// to hiragana and katakana will be equal, so just choose one
 	// to highlight so that we only end up with one pair of strong tags
-	if hiraganaHighlighted == katakanaHighlighted {
-		w.JapaneseHL = strings.Replace(w.Japanese, h, hiraganaHighlighted, -1)
-	} else {
-		// The original input is romaji, so we convert it to hiragana and katakana
-		// and highlight both.
-		w.JapaneseHL = strings.Replace(w.Japanese, h, hiraganaHighlighted, -1)
+	w.JapaneseHL = strings.Replace(w.Japanese, h, hiraganaHighlighted, -1)
+	if hiraganaHighlighted != katakanaHighlighted {
 		w.JapaneseHL = strings.Replace(w.JapaneseHL, k, katakanaHighlighted, -1)
 	}
 
 	// highlight the furigana too, same as above
 	w.FuriganaHL = strings.Replace(w.Furigana, h, hiraganaHighlighted, -1)
-	w.FuriganaHL = strings.Replace(w.FuriganaHL, k, katakanaHighlighted, -1)
+	if k != h {
+		w.FuriganaHL = strings.Replace(w.FuriganaHL, k, katakanaHighlighted, -1)
+	}
+
 	// highlight the query inside the list of English definitions
 	w.EnglishHL = []string{}
 	for _, e := range w.English {
