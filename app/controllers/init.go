d2983fa99b50c54473920034ab2789b9d2517c8b
Herman Schaaf <hermanschaaf@gmail.com>
2013-11-09 18:46:16 +0900

    Add html cleaning function as workaround

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -4,12 +4,18 @@ import (
 	"github.com/jgraham909/revmgo"
 	"github.com/robfig/revel"
 	"html/template"
+	"strings"
 )
 
 func init() {
 	revel.TemplateFuncs["add"] = func(a, b int) int { return a + b }
 	revel.TemplateFuncs["get"] = func(a []string, b int) string { return a[b] }
 	revel.TemplateFuncs["html"] = func(s string) template.HTML { return template.HTML(s) }
+	revel.TemplateFuncs["clean"] = func(s string) string {
+		s = strings.Replace(s, `<strong>`, ``, -1)
+		s = strings.Replace(s, `</strong>`, ``, -1)
+		return s
+	}
 	revel.TemplateFuncs["contains"] = func(a string, b []string) bool {
 		for i := range b {
 			if a == b[i] {
