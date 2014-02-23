e26a8cf867f05f4b6a4019f49c12f9246e9ec1bb
Shawn Smith <shawnpsmith@gmail.com>
2014-02-23 14:34:11 +0900

    update populate scripts

--- scripts/populate-with-files.sh
+++ scripts/populate-with-files.sh
@@ -2,13 +2,25 @@
 
 if [ $# -eq 0 ]
   then
-    echo "output JSON from edict2_parser.py is required"
+      echo "output JSON from populate.py is required (./populate.py print)"
 fi
 
+echo "Deleting current edict index..."
 curl -XDELETE 'http://localhost:9200/edict/'
 
-split $1 -l 2000
+if [ -d "json" ]; then
+  rm json/*
+else
+  mkdir json
+fi
 
+echo "Splitting JSON output into 2000 files..."
+cd json && split ../$1 -l 2000
+
+# Append a newline to each file
 for i in `ls`; do echo ''>>$i; done;
 
+echo "Bulk inserting into ES..."
 for i in `ls`; do curl -s -XPOST "localhost:9200/_bulk" --data-binary @$i; done;
+
+echo "Done!\n"
