8629c037452ffe24033d14fde0c89a302f0a181d
Shawn Smith <shawnpsmith@gmail.com>
2015-05-10 16:21:03 +0900

    attempt to fix everything (not working yet)

---
+++ lib/dictionary/radix_tree_test.go
@@ -0,0 +1,25 @@
+package dictionary
+
+import "testing"
+
+var getTests = []string{"しけん",
+	"てつだう",
+	"手伝う",
+	"ふつう",
+	"普通",
+	//"ふつ",
+}
+
+func TestGet(t *testing.T) {
+	r := NewRadixTree()
+	for i, entry := range getTests {
+		r.Insert(entry, EntryID(i))
+		got := r.Get(entry)
+		if len(got) != 1 {
+			t.Fatalf("%q len(got) = %d, want %d", entry, len(got), 1)
+		}
+		if got[0] != EntryID(i) {
+			t.Fatalf("got[0] = %q, want %q", got[0], entry)
+		}
+	}
+}
